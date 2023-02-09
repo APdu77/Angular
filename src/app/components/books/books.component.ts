9edb3854f16ca9c2cca6a64beed3cdf0599e20b0
Michael CORNILLON <ei.dwaps@gmail.com>
2023-02-09 17:24:24 +0100

    ex livres: création du service book

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Author } from 'src/app/models/author';
 import { Book } from 'src/app/models/book';
 
 @Component({
@@ -8,26 +7,6 @@ import { Book } from 'src/app/models/book';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent {
-  description = "Quos explicabo laudantium hic. Amet dolorem et in et quibusdam eligendi provident ipsam. Et ipsa nulla aspernatur quisquam facilis nulla beatae eveniet.";
-  books = [
-    new Book(
-      crypto.randomUUID(),
-      "Il était une fois",
-      new Author("Coco", "COCO"),
-      this.description,
-      15),
-    new Book(
-      crypto.randomUUID(),
-      "Il était deux fois",
-      new Author("Jaco", "JACO"),
-      this.description,
-      76.5),
-    new Book(
-      crypto.randomUUID(),
-      "Il était trois fois",
-      new Author("Pedro", "PEDRO"),
-      this.description,
-      2.3),
-  ];
+  books: Book[] = [];
   selected: any;
 }
---
+++ src/app/services/book.service.ts
@@ -0,0 +1,25 @@
+import { Author } from '../models/author';
+import { Book } from '../models/book';
+
+export class BookService {
+  public books = [
+    new Book(
+      crypto.randomUUID(),
+      "Il était une fois",
+      new Author("Coco", "COCO"),
+      "Quos explicabo laudantium hic. Amet dolorem et in",
+      15),
+    new Book(
+      crypto.randomUUID(),
+      "Il était deux fois",
+      new Author("Jaco", "JACO"),
+      "Quos explicabo laudantium hic. Amet dolorem et in",
+      76.5),
+    new Book(
+      crypto.randomUUID(),
+      "Il était trois fois",
+      new Author("Pedro", "PEDRO"),
+      "Quos explicabo laudantium hic. Amet dolorem et in",
+      2.3),
+  ];
+}
