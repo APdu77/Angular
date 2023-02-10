9261e0a0aef319aa6141d2b0915955f69cf705a0
Michael CORNILLON <ei.dwaps@gmail.com>
2023-02-10 16:54:20 +0100

    structure de démarrage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BookDetailsComponent } from './components/books/book-details/book-details.component';
-import { BooksListComponent } from './components/books/books-list/books-list.component';
-import { BooksComponent } from './components/books/books.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent,
-    BookDetailsComponent,
-    BooksListComponent,
-    HeaderComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule
--- src/app/models/author.ts
+++
@@ -1,6 +0,0 @@
-export class Author {
-  constructor(
-    public fn: string,
-    public ln: string,
-  ) {}
-}
