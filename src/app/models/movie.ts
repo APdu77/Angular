b24f9b77751f0a67eb652a069b8d6c062774f437
Michael CORNILLON <ei.dwaps@gmail.com>
2023-02-13 10:51:00 +0100

    ex movies: modèle et service

--- src/app/models/movie.ts
+++ src/app/models/movie.ts
@@ -1,2 +1,8 @@
 export class Movie {
+  constructor(
+    public id: string,
+    public title: string,
+    public desc: string,
+    public img: string,
+  ) {}
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Movie } from '../models/movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-
-  constructor() { }
+  public movies: Movie[] = [
+    new Movie(
+      crypto.randomUUID(),
+      'Il était une fois',
+      'Earum explicabo possimus dolorem velit necessitatibus quibusdam mollitia voluptatem.',
+      'http://placeimg.com/640/480/abstract'
+    ),
+    new Movie(
+      crypto.randomUUID(),
+      'Biographie d\'un géologue',
+      'Omnis eum commodi voluptatum sunt odit dolor ex.',
+      'http://placeimg.com/640/480/food'
+    ),
+    new Movie(
+      crypto.randomUUID(),
+      'Il était une fois',
+      'Voluptatem delectus qui eos error. Et accusamus et.',
+      'http://placeimg.com/640/480/business'
+    ),
+  ];
 }
