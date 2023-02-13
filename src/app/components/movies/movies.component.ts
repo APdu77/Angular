086c04b64818d4d46eafbdd87303fbbefba1a005
Michael CORNILLON <ei.dwaps@gmail.com>
2023-02-13 11:24:51 +0100

    ex movies: affichage liste des films

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Movie } from 'src/app/models/movie';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-movies',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent {
-
+  movies: Movie[] = this._movieService.movies;
+  constructor(private _movieService: MovieService) {}
 }
