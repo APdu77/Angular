33c3262b2a319142cbecea82900bc226285d7ab4
Michael CORNILLON <ei.dwaps@gmail.com>
2023-02-13 10:47:12 +0100

    ex movies: créations des composants, du modèle et du service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MovieListComponent } from './components/movies/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './components/movies/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MoviesComponent,
+    MovieListComponent,
+    MovieDetailsComponent,
   ],
   imports: [
     BrowserModule,
